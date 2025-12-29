0d415d5fb0cc8d3bba92c928ab822beb1f19cd95
Leo <leovilis@gmail.com>
2025-12-29 10:35:47 -0300

    BANNER DE AÑO NUEVO

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,6 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-/*import ChristmasBanner from './components/ChristmasBanner'*/
-/*import BlackFridayBanner from './components/BlackFridayBanner'*/
 import NewYearBanner from './components/NewYearBanner'
 const inter = Inter({ subsets: ['latin'] })
 
